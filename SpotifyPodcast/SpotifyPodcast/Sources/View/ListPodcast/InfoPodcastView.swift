118c0d2a9c2d397a617c6d3d6bb3ffc674272f0a
Denis Ostapiv <denisostapiv.dev@gmail.com>
2025-04-24 18:27:39 +0300

    Refactored the view

--- SpotifyPodcast/SpotifyPodcast/Sources/View/ListPodcast/InfoPodcastView.swift
+++ SpotifyPodcast/SpotifyPodcast/Sources/View/ListPodcast/InfoPodcastView.swift
@@ -16,60 +16,26 @@ struct InfoPodcastView: View {
     var body: some View {
         ScrollView {
             VStack {
-                switch podcast.image {
-                case .remoute(let url):
-                    KFImage(url)
-                        .resizable()
-                        .placeholder {
-                            ProgressView()
-                        }
-                        .scaledToFit()
-                        .frame(width: 300, height: 300)
-                        .cornerRadius(4)
-                    
-                case .local(let imageName):
-                    Image(imageName) //інша іконка для відсутнього зображення
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 300, height: 300)
-                }
+                image
             }
             .padding(.bottom, 16)
             
-            Text(podcast.title)
-                .font(.system(size: 24))
-                .fontWeight(.bold)
-                .multilineTextAlignment(.leading)
-                .padding(.bottom, 10)
-                .frame(maxWidth: .infinity, alignment: .leading)
+            title
             
             HStack (spacing:10) {
-                Text("\(podcast.duration) m")
-                    .font(.system(size: 14))
+                duration
                 Spacer()
-                Text(podcast.releaseDate)
-                    .font(.system(size: 14))
+                releaseDate
             }
             .padding(.bottom, 10)
             
             VStack {
-                HStack(spacing:12){
-                    Image(systemName: "star")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Image(systemName: "square.and.arrow.down")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Image(systemName: "square.and.arrow.up")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                    Image(systemName: "ellipsis")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
+                HStack(spacing:12) {
+                    favoriteIcon
+                    downloadedIcon
+                    shareIcon
+                    moreIcon
+                    
                     Spacer()
                     
                     Button(action: {
@@ -77,12 +43,8 @@ struct InfoPodcastView: View {
                     }) {
                         
                         HStack {
-                            Image(systemName: "play.circle.fill")
-                                .foregroundColor(.white)
-                            Text("Play")
-                                .font(.system(size: 18))
-                                .fontWeight(.bold)
-                                .foregroundColor(.white)
+                            buttonImage
+                            buttonText
                         }
                         .padding()
                         .frame(width: 120, height: 48)
@@ -92,9 +54,9 @@ struct InfoPodcastView: View {
                 }
             }
             .padding(.bottom, 10)
+            
             VStack {
-                Text(podcast.description)
-                    .multilineTextAlignment(.leading)
+                description
             }
             .padding(.bottom, 10)
             .frame(maxWidth: .infinity, alignment: .leading)
@@ -109,6 +71,97 @@ struct InfoPodcastView: View {
             if let player = viewModel.player {
                 AudioPlayerView(player: player)
             }
+        }
+    }
+}
+
+private extension InfoPodcastView {
+    
+    var title: some View {
+        Text(podcast.title)
+            .font(.system(size: 24))
+            .fontWeight(.bold)
+            .multilineTextAlignment(.leading)
+            .padding(.bottom, 10)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    var duration: some View {
+        Text("\(podcast.duration) m")
+            .font(.system(size: 14))
+    }
+    
+    var releaseDate: some View {
+        Text(podcast.releaseDate)
+            .font(.system(size: 14))
+    }
+    
+    var favoriteIcon: some View {
+        Image(systemName: "star")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
+    
+    var downloadedIcon: some View {
+        Image(systemName: "square.and.arrow.down")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
+    
+    var shareIcon: some View {
+        Image(systemName: "square.and.arrow.up")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
+    
+    var moreIcon: some View {
+        Image(systemName: "ellipsis")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
+    
+    var description: some View {
+        Text(podcast.description)
+            .multilineTextAlignment(.leading)
+    }
+    
+    var buttonImage: some View {
+        Image(systemName: "play.circle.fill")
+            .foregroundColor(.white)
+    }
+    
+    var buttonText: some View {
+        Text("Play")
+            .font(.system(size: 18))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+    }
+    
+    var image: some View {
+        switch podcast.image {
+        case .remoute(let url):
+            return AnyView(
+                KFImage(url)
+                    .resizable()
+                    .placeholder {
+                        ProgressView()
+                    }
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .cornerRadius(4)
+            )
+        case .local(let imageName):
+            return AnyView(
+                Image(imageName) //інша іконка для відсутнього зображення
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .cornerRadius(4)
+            )
         }
     }
 }
